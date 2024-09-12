0d662555492574f137d816d88f308f2f320c9156
OfirDataloopAI <ofir.g@dataloop.ai>
2024-09-12 11:50:56 +0300

    parser in progress

--- annotations_parser.py
+++ annotations_parser.py
@@ -2,6 +2,7 @@ import dtlpy as dl
 import os
 import pathlib
 import json
+from tqdm import tqdm
 
 
 def annotations_uploader(dataset: dl.Dataset):
@@ -15,47 +16,59 @@ def annotations_uploader(dataset: dl.Dataset):
 
     text_files = pathlib.Path(download_path).rglob('*.txt')
     for text_file in text_files:
+        # Read annotations from text file
+        print(f"Processing file: {text_file}")
         with open(text_file, 'r') as f:
             annotations = f.readlines()
         annotations = [annotation.strip() for annotation in annotations]
         # print(f"Annotations: {annotations}")
 
+        # Read images dir from annotations json file
         text_file_json_filepath = str(text_file).replace('items', 'json')
         text_file_json_filepath = text_file_json_filepath.replace('.txt', '.json')
         with open(text_file_json_filepath, 'r') as f:
             text_file_json = json.load(f)
         images_dir = text_file_json["dir"].replace("gt", "img1")
 
+        # Get all video frames
         filters = dl.Filters()
         filters.add(field=dl.FiltersKnownFields.DIR, values=images_dir)
         filters.sort_by(field=dl.FiltersKnownFields.FILENAME, value=dl.FiltersOrderByDirection.ASCENDING)
         items = list(dataset.items.list(filters=filters).all())
 
+        # Create annotation builders
         item_builders: [dl.AnnotationCollection] = list()
         item: dl.Item
         for item in items:
             builder = item.annotations.builder()
             item_builders.append(builder)
 
+        # Add annotations to builders
         for annotation in annotations:
             # Annotation format: <frame>, <id>, <bb_left>, <bb_top>, <bb_width>, <bb_height>, 1, 1, 1
             frame, object_id, bb_left, bb_top, bb_width, bb_height, _, _, _ = annotation.split(',')
-            bb_right = bb_left + bb_width
-            bb_bottom = bb_top + bb_height
+            top = bb_top - (bb_height // 2)
+            left = bb_left - (bb_width // 2)
+            bottom = bb_top + (bb_height // 2)
+            right = bb_left + (bb_width // 2)
 
             item_builders[int(frame) - 1].add(
                 annotation_definition=dl.Box(
                     label="person",
-                    top=bb_top,
-                    left=bb_left,
-                    bottom=bb_bottom,
-                    right=bb_right
+                    top=top,
+                    left=left,
+                    bottom=bottom,
+                    right=right
                 ),
                 object_id=object_id
             )
 
-        for builder in item_builders:
+        # Upload annotations
+        print(f"Uploading annotations for {len(item_builders)} items...")
+        for builder in tqdm(item_builders):
             builder.upload()
+
+            exit()
 
 
 def main():
